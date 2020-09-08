57273f71c38adbfb66426cbca99693037ad203d3
xrotwang <xrotwang@googlemail.com>
2020-09-08 16:07:38 +0200

    sample data and skeleton

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# liljegrenhindukush
+# Language Contact and Relatedness in the Hindukush Region
+
+Available online at https://hindukush.ling.su.se/
+
+
+
+# Contributors
+
+Name | GitHub user | Role
+--- | --- | ---
+ | @ | maintainer
+
+
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+import json
+
+
+with open('metadata.json', encoding='utf-8') as fp:
+    metadata = json.load(fp)
+
+
+setup(
+    name='lexibank_liljegrenhindukush',
+    description=metadata['title'],
+    license=metadata.get('license', ''),
+    url=metadata.get('url', ''),
+    py_modules=['lexibank_liljegrenhindukush'],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'lexibank.dataset': [
+            'liljegrenhindukush=lexibank_liljegrenhindukush:Dataset',
+        ]
+    },
+    install_requires=[
+        'pylexibank>=2.7.1',
+    ],
+    extras_require={
+        'test': [
+            'pytest-cldf',
+        ],
+    },
+)
